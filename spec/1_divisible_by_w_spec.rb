ad9146d454ae5ecafe2ba5e1ec95f6b084b97e5a
PhilipeeX <dev.philipe@outlook.com>
2023-08-21 11:41:42 -0300

    test(divisible_by_w): more tests created with rspec

--- spec/1_divisible_by_w_spec.rb
+++ spec/1_divisible_by_w_spec.rb
@@ -1,4 +1,3 @@
-# spec/calculator_spec.rb
 require 'rspec'
 require_relative '../lib/1_divisible_by_w'
 
@@ -7,7 +6,16 @@ describe '#divisible_by_w' do
     expect(divisible_by_w(10, 50, 5)).to eq([15, 20, 25, 30, 35, 40, 45])
   end
 
-  it 'returns an empty array if no numbers are divisible by w between x and y' do
+  it 'returns an empty array if no numbers are divisible by w' do
     expect(divisible_by_w(10, 15, 100)).to eq([])
   end
+
+  it 'returns an empty array when w is larger than the range between x and y' do
+    expect(divisible_by_w(10, 15, 20)).to eq([])
+  end
+
+  it 'returns numbers divisible by w for a non-trivial divisor' do
+    expect(divisible_by_w(10, 50, 7)).to eq([14, 21, 28, 35, 42, 49])
+  end
+
 end
