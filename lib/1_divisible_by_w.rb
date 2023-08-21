80e33a8172abbab186da4c1d17188877438f077b
PhilipeeX <dev.philipe@outlook.com>
2023-08-21 11:41:21 -0300

    refactor(divisible_by_w):

--- lib/1_divisible_by_w.rb
+++ lib/1_divisible_by_w.rb
@@ -1,22 +1,4 @@
 def divisible_by_w(x, y, w)
-
-  if x >= y || x == y - 1
-    return nil
-  else
-    divisible = []
-    i = x + 1
-    while i < y
-      if i % w == 0
-        divisible << i
-      end
-      i += 1
-    end
-    puts divisible
-  end
-
+  return nil if x >= y || x == y - 1
+  (x+1...y).select { |num| num % w == 0 }
 end
-
-puts divisible_by_w(2, 12, 2)
-puts divisible_by_w(2, 12, 3)
-p divisible_by_w(12, 12, 2)
-p divisible_by_w(22, 1, 2)
